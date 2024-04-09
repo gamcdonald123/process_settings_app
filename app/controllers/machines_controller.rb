195d8975e6be6118565c16f745c905642d04b7cb
gamcdonald123 <gamcdonald@icloud.com>
2024-04-09 17:33:16 +0100

    Started work on adding a machine

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,10 +1,30 @@
 class MachinesController < ApplicationController
 
+  before_action :set_machine, only: [:show]
+
   def index
     @machines = Machine.all
   end
 
   def show
+  end
+
+  def new
+    @machine = Machine.new
+  end
+
+  def create
+    @machine = Machine.new(machine_params)
+  end
+
+  private
+
+  def machine_params
+    params.require(:machine).permit(:machine_name, :tonnage, :manufacturer)
+  end
+
+  def set_machine
     @machine = Machine.find(params[:id])
   end
+
 end
