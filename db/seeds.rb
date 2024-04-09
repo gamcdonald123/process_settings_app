fbceaee25bf028998748a15efc7b896dbd17e8e0
gamcdonald123 <gamcdonald@icloud.com>
2024-04-09 11:22:44 +0100

    Adding previous work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Setting.destroy_all
+Tool.destroy_all
+Machine.destroy_all
+Technician.destroy_all
+Site.destroy_all
+
+Site.create(site_name: "GFC")
+Site.create(site_name: "GPH")
+Site.create(site_name: "GNW")
