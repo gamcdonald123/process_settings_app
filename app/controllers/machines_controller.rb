3192d55d33d1a6bf9f45d4530a598735091600e5
gamcdonald123 <gamcdonald@icloud.com>
2024-04-10 12:21:28 +0100

    Added more views and can create new machine. Machines ordered by tonnage.

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -3,10 +3,12 @@ class MachinesController < ApplicationController
   before_action :set_machine, only: [:show]
 
   def index
-    @machines = Machine.all
+    @machines = Machine.all.order(:tonnage)
   end
 
   def show
+    # create @settings by finding all settings associated with the machine
+    @settings = @machine.settings
   end
 
   def new
@@ -15,12 +17,18 @@ class MachinesController < ApplicationController
 
   def create
     @machine = Machine.new(machine_params)
+
+    if @machine.save
+      redirect_to @machine, notice: 'Machine was successfully created.'
+    else
+      render :new
+    end
   end
 
   private
 
   def machine_params
-    params.require(:machine).permit(:machine_name, :tonnage, :manufacturer)
+    params.require(:machine).permit(:machine_name, :tonnage, :manufacturer, :site_id)
   end
 
   def set_machine
--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,9 +2,19 @@ class SettingsController < ApplicationController
 
   def index
     @settings = Setting.all
+    @tool = Tool.all
   end
 
   def show
+  end
+
+  private
+
+  def setting_params
+    params.require(:setting).permit()
+  end
+
+  def set_setting
     @setting = Setting.find(params[:id])
   end
 end
