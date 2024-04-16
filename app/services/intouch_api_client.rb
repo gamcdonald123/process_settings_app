8480747752eb743ed67e88af99303fb2bc71ecfe
gamcdonald123 <gamcdonald@icloud.com>
2024-04-16 14:12:12 +0100

    Added machine status function to Intouch service.

--- app/services/intouch_api_client.rb
+++ app/services/intouch_api_client.rb
@@ -4,11 +4,12 @@ require 'json'
 class IntouchApiClient
   def initialize(token: ENV['INTOUCH_API_TOKEN'])
     @token = token
+    @base_url = "https://i4api.azurewebsites.net/api"
   end
 
   # Fetch a list of all the machines
   def fetch_machines
-    uri = URI("https://i4api.azurewebsites.net/api/Machine")
+    uri = URI("#{@base_url}/Machine")
     json_response = URI.open(uri, "Authorization" => "Bearer #{@token}")
     JSON.parse(json_response.read)
   rescue StandardError => e
@@ -36,6 +37,16 @@ class IntouchApiClient
   rescue StandardError => e
     # Handle any exceptions that occur during processing
     { error: "Error parsing machine list: #{e.message}" }
+  end
+
+  # This method isn't live with Intouch's API yet
+  def get_machine_status
+    uri = "#{@base_url}/getmachinestatus/A41A76AE-E749-498B-9934-590F8BAB6F40"
+    puts uri
+    json_response = URI.open(uri, "Authorization" => "Bearer #{@token}")
+    JSON.parse(json_response.read)
+  rescue StandardError => e
+    { error: "Failed to fetch machine: #{e.message}"}
   end
 
 end
