645978d58ecc50780979b2a8af7118568e74e74a
gamcdonald123 <gamcdonald@icloud.com>
2024-04-16 14:12:52 +0100

    Started following gorails tutorial. Ended, due to complexity.

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,7 +2,15 @@ class ToolsController < ApplicationController
 
   def index
     @tools = Tool.all
+    @tools = @tools.search(params[:query]) if params[:query].present?
+    @pagy, @tools = pagy @tools.reorder(sort_column => sort_direction), items: params.fetch(:count, 10)
   end
+
+  def sort_column
+    %w{ name maintainx_id tool_type }.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
 
   def show
     @tool = Tool.find(params[:id])
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,5 @@
 class Tool < ApplicationRecord
   has_many :settings
+  include PgSearch::Model
+  pg_search_scope :search, against: [:name, :tool_type], using: { tsearch: { prefix: true } }
 end
