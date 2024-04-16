85fd201492cf43b56ad9a442f2059794d4a2dd3b
gamcdonald123 <gamcdonald@icloud.com>
2024-04-16 17:07:04 +0100

    Updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,69 +24,69 @@ puts "Sites created."
 
 puts "Creating machines..."
 #GFC Machines
-Machine.create(manufacturer: "Negri Bossi", tonnage: 1000, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 15")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 800, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 14")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 650, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 13A")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 550, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 13")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 500, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 11")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 380, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 10")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 210, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 7")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 90, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 5")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 120, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 4")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 160, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 3")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 160, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 2")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 50, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 1A")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 1000, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 15")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 800, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 14")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 650, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 13A")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 550, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 13")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 500, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 11")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 380, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 10")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 210, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 7")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 90, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 5")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 120, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 4")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 160, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 3")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 160, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 2")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 50, site_id: 1, machine_type: "Injection Moulding", machine_name: "IMM 1A")
 
-#GPH Machines
-Machine.create(manufacturer: "Arburg", tonnage: 50, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 7A")
-Machine.create(manufacturer: "Arburg", tonnage: 100, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 16")
-Machine.create(manufacturer: "Arburg", tonnage: 250, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 17")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 70, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 10")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 70, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 11")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 200, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 3")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 90, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 1")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 270, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 5")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 150, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 2")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 210, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 13")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 200, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 4")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 270, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 6")
-Machine.create(manufacturer: "Arburg", tonnage: 35, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 9")
-Machine.create(manufacturer: "Arburg", tonnage: 35, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 8")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 210, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 12")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 85, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 7")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 180, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 14")
-Machine.create(manufacturer: "Engel", tonnage: 220, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 15")
+# #GPH Machines
+# Machine.create(manufacturer: "Arburg", tonnage: 50, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 7A")
+# Machine.create(manufacturer: "Arburg", tonnage: 100, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 16")
+# Machine.create(manufacturer: "Arburg", tonnage: 250, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 17")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 70, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 10")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 70, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 11")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 200, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 3")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 90, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 1")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 270, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 5")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 150, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 2")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 210, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 13")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 200, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 4")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 270, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 6")
+# Machine.create(manufacturer: "Arburg", tonnage: 35, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 9")
+# Machine.create(manufacturer: "Arburg", tonnage: 35, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 8")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 210, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 12")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 85, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 7")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 180, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 14")
+# Machine.create(manufacturer: "Engel", tonnage: 220, site_id: 2, machine_type: "Injection Moulding", machine_name: "IMM 15")
 
-#GNW Machines
-Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M01")
-Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M02")
-Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M04")
-Machine.create(manufacturer: "Sandretto", tonnage: 270, site_id: 3, machine_type: "Injection Moulding", machine_name: "M30")
-Machine.create(manufacturer: "Sandretto", tonnage: 100, site_id: 3, machine_type: "Injection Moulding", machine_name: "M31")
-Machine.create(manufacturer: "Sandretto", tonnage: 60, site_id: 3, machine_type: "Injection Moulding", machine_name: "M32")
-Machine.create(manufacturer: "Sandretto", tonnage: 60, site_id: 3, machine_type: "Injection Moulding", machine_name: "M33")
-Machine.create(manufacturer: "Sandretto", tonnage: 100, site_id: 3, machine_type: "Injection Moulding", machine_name: "M34")
-Machine.create(manufacturer: "Sandretto", tonnage: 320, site_id: 3, machine_type: "Injection Moulding", machine_name: "M35")
-Machine.create(manufacturer: "Demag", tonnage: 35, site_id: 3, machine_type: "Injection Moulding", machine_name: "M44")
-Machine.create(manufacturer: "Sandretto", tonnage: 85, site_id: 3, machine_type: "Injection Moulding", machine_name: "M45")
-Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M60")
-Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M61")
-Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M62")
-Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M63")
-Machine.create(manufacturer: "Krauss Maffei", tonnage: 160, site_id: 3, machine_type: "Injection Moulding", machine_name: "M65")
-Machine.create(manufacturer: "Krauss Maffei", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M66")
-Machine.create(manufacturer: "Demag", tonnage: 35, site_id: 3, machine_type: "Injection Moulding", machine_name: "M110")
-Machine.create(manufacturer: "Sandretto", tonnage: 95, site_id: 3, machine_type: "Injection Moulding", machine_name: "M306")
-Machine.create(manufacturer: "Sandretto", tonnage: 190, site_id: 3, machine_type: "Injection Moulding", machine_name: "M408")
-Machine.create(manufacturer: "Sandretto", tonnage: 380, site_id: 3, machine_type: "Injection Moulding", machine_name: "M700")
-Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M68")
-Machine.create(manufacturer: "Engel", tonnage: 300, site_id: 3, machine_type: "Injection Moulding", machine_name: "M71")
-Machine.create(manufacturer: "Engel", tonnage: 400, site_id: 3, machine_type: "Injection Moulding", machine_name: "M70")
-Machine.create(manufacturer: "Engel", tonnage: 150, site_id: 3, machine_type: "Injection Moulding", machine_name: "M73")
-Machine.create(manufacturer: "Engel", tonnage: 50, site_id: 3, machine_type: "Injection Moulding", machine_name: "M72")
-Machine.create(manufacturer: "Engel", tonnage: 400, site_id: 3, machine_type: "Injection Moulding", machine_name: "M74")
-Machine.create(manufacturer: "Negri Bossi", tonnage: 500, site_id: 3, machine_type: "Injection Moulding", machine_name: "M75")
-puts "Machines created."
+# #GNW Machines
+# Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M01")
+# Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M02")
+# Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M04")
+# Machine.create(manufacturer: "Sandretto", tonnage: 270, site_id: 3, machine_type: "Injection Moulding", machine_name: "M30")
+# Machine.create(manufacturer: "Sandretto", tonnage: 100, site_id: 3, machine_type: "Injection Moulding", machine_name: "M31")
+# Machine.create(manufacturer: "Sandretto", tonnage: 60, site_id: 3, machine_type: "Injection Moulding", machine_name: "M32")
+# Machine.create(manufacturer: "Sandretto", tonnage: 60, site_id: 3, machine_type: "Injection Moulding", machine_name: "M33")
+# Machine.create(manufacturer: "Sandretto", tonnage: 100, site_id: 3, machine_type: "Injection Moulding", machine_name: "M34")
+# Machine.create(manufacturer: "Sandretto", tonnage: 320, site_id: 3, machine_type: "Injection Moulding", machine_name: "M35")
+# Machine.create(manufacturer: "Demag", tonnage: 35, site_id: 3, machine_type: "Injection Moulding", machine_name: "M44")
+# Machine.create(manufacturer: "Sandretto", tonnage: 85, site_id: 3, machine_type: "Injection Moulding", machine_name: "M45")
+# Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M60")
+# Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M61")
+# Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M62")
+# Machine.create(manufacturer: "Engel", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M63")
+# Machine.create(manufacturer: "Krauss Maffei", tonnage: 160, site_id: 3, machine_type: "Injection Moulding", machine_name: "M65")
+# Machine.create(manufacturer: "Krauss Maffei", tonnage: 80, site_id: 3, machine_type: "Injection Moulding", machine_name: "M66")
+# Machine.create(manufacturer: "Demag", tonnage: 35, site_id: 3, machine_type: "Injection Moulding", machine_name: "M110")
+# Machine.create(manufacturer: "Sandretto", tonnage: 95, site_id: 3, machine_type: "Injection Moulding", machine_name: "M306")
+# Machine.create(manufacturer: "Sandretto", tonnage: 190, site_id: 3, machine_type: "Injection Moulding", machine_name: "M408")
+# Machine.create(manufacturer: "Sandretto", tonnage: 380, site_id: 3, machine_type: "Injection Moulding", machine_name: "M700")
+# Machine.create(manufacturer: "Sandretto", tonnage: 110, site_id: 3, machine_type: "Injection Moulding", machine_name: "M68")
+# Machine.create(manufacturer: "Engel", tonnage: 300, site_id: 3, machine_type: "Injection Moulding", machine_name: "M71")
+# Machine.create(manufacturer: "Engel", tonnage: 400, site_id: 3, machine_type: "Injection Moulding", machine_name: "M70")
+# Machine.create(manufacturer: "Engel", tonnage: 150, site_id: 3, machine_type: "Injection Moulding", machine_name: "M73")
+# Machine.create(manufacturer: "Engel", tonnage: 50, site_id: 3, machine_type: "Injection Moulding", machine_name: "M72")
+# Machine.create(manufacturer: "Engel", tonnage: 400, site_id: 3, machine_type: "Injection Moulding", machine_name: "M74")
+# Machine.create(manufacturer: "Negri Bossi", tonnage: 500, site_id: 3, machine_type: "Injection Moulding", machine_name: "M75")
+# puts "Machines created."
 
 puts "Creating technicians..."
 Technician.create(first_name: "Andy", last_name: "Dicken", site_id: 1)
@@ -101,20 +101,20 @@ puts "Technicians created."
 temps = (100..250).to_a
 
 # create array of numbers between 1 and 6
-technicians = (1..6).to_a
+# technicians = (1..6).to_a
 
-machines = (1..58).to_a
+# machines = (1..58).to_a
 
-tools = (1..9).to_a
+# tools = (1..9).to_a
 
 weights = (1..1000).to_a
 
 puts "Creating settings..."
 100.times do
   Setting.create!(
-    machine_id: machines.sample,
-    # tool_id: rand(1..9),
-    technician_id: technicians.sample,
+    machine_id: Machine.all.sample.id,
+    tool_id: Tool.all.sample.id,
+    technician_id: Technician.all.sample.id,
     shot_weight: weights.sample,
     sprue_weight: weights.sample,
     nozzle_temp: temps.sample,
