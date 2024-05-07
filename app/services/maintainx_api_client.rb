b92128433e5375fa39d24ca6a7b156e42817b131
Guy McDonald <guy@goodfishgroup.com>
2024-05-07 12:12:51 +0100

    refactor: Update MaintainxApiClient to include additional machine details
    This commit updates the MaintainxApiClient class to include additional machine details when creating a new machine record. The `manufacturer`, `manufacture_year`, and `tonnage` fields are now populated from the `extraFields` object in the `machine` parameter.

--- app/services/maintainx_api_client.rb
+++ app/services/maintainx_api_client.rb
@@ -105,8 +105,9 @@ class MaintainxApiClient
                         'Extrusion Line'
                       end,
         site_id: site_id, # Storing the site name or ID as determined above
-        manufacturer: machine['manufacturer'],
-        manufacture_year: machine['year']
+        manufacturer: machine['extraFields']['Manufacturer'],
+        manufacture_year: machine['extraFields']['Year'],
+        tonnage: machine['extraFields']['Tonnage']
       )
 
       machine_count += 1 if new_machine.persisted?
