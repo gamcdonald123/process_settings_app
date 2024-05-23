ad6072bdf5cb67097445d1c95b222bd07ce2e4e9
gamcdonald123 <gamcdonald@icloud.com>
2024-05-23 15:36:54 +0100

    Add whodunnit callback to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_authorization
+  before_action :set_paper_trail_whodunnit
 
   def after_sign_in_path_for(resource)
     if resource.roles.nil?
