ffa12320333c0cb39ef51fc7f0e3d95a8379f06a
Guy McDonald <guy@goodfishgroup.com>
2024-05-30 15:59:11 +0100

    Finished updating the form

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -19,6 +19,7 @@ class SettingsController < ApplicationController
   def create
     @setting = Setting.new(setting_params)
     if @setting.save
+      @site_id = @setting.tool.site_id
       @version = PaperTrail::Version.where(item_id: @setting.id).last
       if @version.event == 'create'
         @version.object = @setting.attributes.to_yaml
